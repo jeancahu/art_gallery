27bcf9f0ed90975cddf30901d1b1319e1d296a7e
Jeancarlo Hidalgo <jeancahu@gmail.com>
2020-09-24 09:27:03 -0600

    add: author models

--- author/models.py
+++ author/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=256)
+    email = models.EmailField()
+    biography = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
