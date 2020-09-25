a3aaf436e5b58e6755b9113ec040e6b5bd9d5253
Jeancarlo Hidalgo <jeancahu@gmail.com>
2020-09-24 19:19:02 -0600

    chg: collection is a String now

--- artwork/models.py
+++ artwork/models.py
@@ -4,7 +4,7 @@ class Artwork(models.Model):
     name = models.CharField(max_length=256)
     img_url = models.CharField(max_length=256)
     description = models.CharField(max_length=256)
-    collection = models.IntegerField() # TODO chg by collection object list
+    collection = models.CharField(max_length=256) # TODO chg by collection object list
     # author = models.IntegerField() # TODO chg by collection object list
 
     def __str__(self):
--- collection/models.py
+++ collection/models.py
@@ -5,6 +5,7 @@ class Collection(models.Model):
     # category = models.CharField(max_length=256) # TODO subtitle gray
     banner_url = models.CharField(max_length=256)
     description = models.CharField(max_length=256)
+    # created = models.DateTimeField(auto_now_add=True)
     # collection = models.IntegerField() # TODO chg by collection object list
 
     def __str__(self):
